8af8fb84dfb6857aff7e1fb3ec122df73407efef
Ionut Spalatelu <ionutspalatelu@gmail.com>
2019-12-05 22:05:28 +0200

    Observer pattern
    Signed-off-by: Ionut Spalatelu <ionut.spalatelu@gohenry.co.uk>

--- src/main/java/ionut/training/oop/behavioral/observer/ObserverWithGUI.java
+++ src/main/java/ionut/training/oop/behavioral/observer/ObserverWithGUI.java
@@ -1,5 +1,8 @@
 package ionut.training.oop.behavioral.observer;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import ionut.training.MyFrame;
 
 import static javax.swing.JOptionPane.showMessageDialog;
@@ -10,11 +13,19 @@ public class ObserverWithGUI {
         MyFrame frame = new MyFrame();
 
         //    public void actionPerformed(ActionEvent e);
-        frame.button.addActionListener(event ->
-                showMessageDialog(null, "He clicked me"));
-        frame.button.addActionListener(event ->
-                System.out.println("He clicked me in console"));
+        ActionListener clicked_me = event -> showMessageDialog(null, "He clicked me");
+        frame.button.addActionListener(clicked_me);
 
+
+        frame.button.addActionListener(new MyActionListener());
+
+    }
+}
+
+class MyActionListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("He clicked me in console");
 
     }
 }
